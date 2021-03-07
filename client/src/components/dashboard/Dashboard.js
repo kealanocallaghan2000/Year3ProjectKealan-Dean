bfd101f5888b27d67766f5a747c861df1a2ecc41
Dean0 McGowan <G00369547@gmit.ie>
2021-03-07 15:59:57 +0000

    Basic landing, products, and cart page added. Log In and Register Buttons broken

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+
+import AddProduct from '../shop/AddProduct';
+import Cart from '../shop/Cart';
+import ProductList from '../shop/ProductList';
+
+import Context from "../../Context";
 
 class Dashboard extends Component {
   onLogoutClick = e => {
@@ -9,35 +17,130 @@ class Dashboard extends Component {
     this.props.logoutUser();
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+
+  async componentDidMount() {
+    let cart = localStorage.getItem("cart");
+  
+    const products = await axios.get('http://localhost:3001/products');
+    cart = cart? JSON.parse(cart) : {};
+  
+    this.setState({  products: products.data, cart });
+  }
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  };
+
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
+    }
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  removeFromCart = cartItemId => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+  
+  clearCart = () => {
+    let cart = {};
+    localStorage.removeItem("cart");
+    this.setState({ cart });
+  };
+
   render() {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+      
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+        <div className="App">
+          <nav
+            className="navbar container"
+            role="navigation"
+            aria-label="main navigation"
+          >
+            <div className="navbar-brand">
+              <b className="navbar-item is-size-4 ">ecommerce</b>
+              <label
+                role="button"
+                class="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState({ showMenu: !this.state.showMenu });
+                }}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </label>
+            </div>
+              <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    { Object.keys(this.state.cart).length }
+                  </span>
+                </Link>
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path="/dashboard" component={ProductList} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
           </div>
-        </div>
-      </div>
+        </Router>
+      </Context.Provider>
+      
+
     );
   }
 }
